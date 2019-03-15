d5fd774f44a395b9614679405861f935cc2f76d7
Taylor Vesely <tvesely@pivotal.io>
2019-03-15 14:05:03 -0700

    Build/test 5X_STABLE

--- docker_run_icw.sh
+++ docker_run_icw.sh
@@ -5,8 +5,10 @@ GIT_REPO=$1
 GIT_BRANCH=$2
 MAKE_COMMAND=${3:-"-k PGOPTIONS='-c optimizer=on' installcheck-world"}
 
-DOCKER_BUILD_IMAGE=pivotaldata/gpdb6-centos7-build:latest
-DOCKER_TEST_IMAGE=pivotaldata/gpdb6-centos7-test:latest
+GP5X_DOCKER_BUILD_IMAGE="pivotaldata/centos-gpdb-dev:7-gcc6.2-llvm3.7"
+GP5X_DOCKER_TEST_IMAGE="pivotaldata/centos-gpdb-dev:7-gcc6.2-llvm3.7"
+GP6X_DOCKER_BUILD_IMAGE="pivotaldata/gpdb6-centos7-build:latest"
+GP6X_DOCKER_TEST_IMAGE="pivotaldata/gpdb6-centos7-test:latest"
 
 TEMP_GPDB_DOCKER_REPO=/tmp/gpdb_temp_docker_repo_$$
 
@@ -20,18 +22,31 @@ function finish {
 }
 
 build_gpdb() {
+    GPDB_VERSION=$1
+
 	if [ -d ${GPADDON_TEMP_REPO} ]; then
 		rm -rf ${GPADDON_TEMP_REPO}
 	fi
 
 	git clone git@github.com:pivotal/gp-addon ${GPADDON_TEMP_REPO}
 
-	gsutil -m cp gs://gp-internal-artifacts/centos7/{sigar,lib,python}* ${ARTIFACT_DIR}
+    
+    if [ "${GPDB_VERSION}" -eq "5" ]; then
+        DOCKER_BUILD_IMAGE=${GP5X_DOCKER_BUILD_IMAGE}
+        pushd ${GPADDON_TEMP_REPO}
+            git checkout 5X_STABLE
+        popd
 
-	LIBQUICKLZ_RPM=$(basename `ls $ARTIFACT_DIR/libquicklz-1*.rpm`)
-	LIBQUICKLZ_DEVEL_RPM=$(basename `ls $ARTIFACT_DIR/libquicklz-devel*.rpm`)
-	LIBSIGAR_RPM=$(basename `ls $ARTIFACT_DIR/sigar*.targz`)
-	PYTHON_TGZ=$(basename `ls $ARTIFACT_DIR/python*.tar.gz`)
+    elif [ "${GPDB_VERSION}" -eq "6" ]; then
+        gsutil -m cp gs://gp-internal-artifacts/centos7/{sigar,lib,python}* ${ARTIFACT_DIR}
+
+        LIBQUICKLZ_RPM=$(basename `ls $ARTIFACT_DIR/libquicklz-1*.rpm`)
+        LIBQUICKLZ_DEVEL_RPM=$(basename `ls $ARTIFACT_DIR/libquicklz-devel*.rpm`)
+        LIBSIGAR_RPM=$(basename `ls $ARTIFACT_DIR/sigar*.targz`)
+        PYTHON_TGZ=$(basename `ls $ARTIFACT_DIR/python*.tar.gz`)
+
+        DOCKER_BUILD_IMAGE=${GP6X_DOCKER_BUILD_IMAGE}
+    fi
 
 
 	docker pull ${DOCKER_BUILD_IMAGE}
@@ -45,7 +60,6 @@ build_gpdb() {
 	  -v ${ARTIFACT_DIR}/${LIBSIGAR_RPM}:/libsigar-installer/${LIBSIGAR_RPM} \
 	  -v ${ARTIFACT_DIR}/${PYTHON_TGZ}:/python-tarball/${PYTHON_TGZ} \
 	  -v /tmp/artifacts:/gpdb_artifacts \
-	  -v /tmp/ivy_tars:/opt/releng/tools/third-party/ext/gpdb5_ext-3.4/targzs/ \
 	  -e IVYREPO_PASSWD="${IVYREPO_PASSWD}" \
 	  -e IVYREPO_USER="${IVYREPO_USER}" \
 	  -e IVYREPO_REALM="${IVYREPO_REALM}" \
@@ -59,12 +73,18 @@ build_gpdb() {
 	  -e OUTPUT_ARTIFACT_DIR=gpdb_artifacts/${GPDB_SHA} \
 	  ${DOCKER_BUILD_IMAGE} \
 	  bash -c "mkdir /gpAux_ext/ &&
-				cp -R /gpdb_repo gpdb_src
+				cp -R /gpdb_repo gpdb_src &&
 				gpdb_src/concourse/scripts/sync_tools.bash &&
 				gpdb_src/concourse/scripts/compile_gpdb.bash"
 }
 
 test_gpdb() {
+    if [ "${GPDB_VERSION}" -eq "5" ]; then
+        DOCKER_TEST_IMAGE=${GP5X_DOCKER_TEST_IMAGE}
+    elif [ "${GPDB_VERSION}" -eq "6" ]; then
+        DOCKER_TEST_IMAGE=${GP6X_DOCKER_TEST_IMAGE}
+    fi
+
 	docker pull ${DOCKER_TEST_IMAGE}
 
 	DOCKER_CONTAINER=$(docker run -it --rm -d \
@@ -96,12 +116,9 @@ main() {
 	git clone ${GIT_REPO} ${TEMP_GPDB_DOCKER_REPO}
 	pushd ${TEMP_GPDB_DOCKER_REPO}
 	  git checkout ${GIT_BRANCH}
+      git submodule update --init --recursive
 	  GPDB_SHA=$(git rev-parse HEAD)
-	  git submodule init &&
-	  git submodule update --recursive
-	  pushd gpAux/extensions/pgbouncer/source
-		git submodule init && git submodule update
-	  popd
+      GPDB_VERSION=$(expr "`./getversion`" : "\(^[0-9]\)\.[0-9]\+\.[0-9]\+")
 	popd
 
 
@@ -109,7 +126,7 @@ main() {
 
 	[ -d ${ARTIFACT_DIR}/$GPDB_SHA ] ||	mkdir ${ARTIFACT_DIR}/$GPDB_SHA
 
-	[ -f ${ARTIFACT_DIR}/$GPDB_SHA/bin_gpdb.tar.gz ] ||	build_gpdb
+	[ -f ${ARTIFACT_DIR}/$GPDB_SHA/bin_gpdb.tar.gz ] ||	build_gpdb ${GPDB_VERSION}
 
 	test_gpdb
 }
