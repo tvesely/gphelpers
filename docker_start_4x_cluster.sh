5397ad5e57e6f14b9ce3b969b42a139a2b33b976
Taylor Vesely <tvesely@pivotal.io>
2019-04-08 15:06:17 -0700

    docker_start_4x_cluster: Download version if not available

--- docker_start_4x_cluster.sh
+++ docker_start_4x_cluster.sh
@@ -3,20 +3,22 @@ set -ex
 
 source $(dirname $0)/common.sh
 
-
 GIT_REPO=~/workspace/gpdb4
 GIT_BRANCH=4.3_STABLE
 GPDB_VERSION_TO_RUN=$1
 
 TRANSFER_DIR=/tmp/${GPDB_VERSION_TO_RUN}_docker
 
-GPDB_BIN_DIR=~/platform/gpdb/rhel5/bin/
-
 start_gpdb() {
     GPDB_VERSION=$1
 
-    docker pull ${DOCKER_RUN_IMAGE}
+    GPDB_BIN_DIR=/tmp/gpdb_bin_dir_$$
+    mkdir ${GPDB_BIN_DIR}
+    pushd ${GPDB_BIN_DIR}
+        unzip ${GPDB_4X_ZIP_DIRECTORY}/greenplum-db-${GPDB_VERSION}*rhel5*.zip
+    popd
 
+    docker pull ${DOCKER_TEST_IMAGE}
 
     DOCKER_CONTAINER=$(docker run -it --rm -d \
       -v ${TEMP_GPDB_DOCKER_REPO}:/gpdb_repo \
@@ -28,12 +30,13 @@ start_gpdb() {
           /bin/bash
     )
 
+
     SKIP=`awk '/^__END_HEADER__/ {print NR + 1; exit 0; }' ${GPDB_BIN_DIR}/greenplum-db-${GPDB_VERSION}-rhel5-x86_64.bin`
 
     docker exec -it ${DOCKER_CONTAINER} \
     bash -c "cp -R /gpdb_repo /gpdb_src
        mkdir /usr/local/greenplum-db-devel/
-       tail -n +${SKIP} /bin_gpdb/greenplum-db-4.3.29.0-rhel5-x86_64.bin | tar zxf - -C /usr/local/greenplum-db-devel/
+       tail -n +${SKIP} /bin_gpdb/greenplum-db-${GPDB_VERSION}*rhel5*.bin | tar zxf - -C /usr/local/greenplum-db-devel/
        gpdb_src/ci/concourse/scripts/setup_gpadmin_user.bash centos
        source gpdb_src/ci/concourse/scripts/common.bash
        time setup_gpadmin_user
@@ -43,6 +46,8 @@ start_gpdb() {
        echo container=${DOCKER_CONTAINER} || \
        echo "ICW Failed on container ${DOCKER_CONTAINER}" && \
        echo "   HINT: docker exec -it ${DOCKER_CONTAINER}"
+
+    rm ${GPDB_BIN_DIR}
 }
 
 main() {
@@ -52,7 +57,9 @@ main() {
         mkdir ${TRANSFER_DIR}
     fi
 
-    if [ -f ${GPDB_BIN_DIR}/greenplum-db*${GPDB_VERSION_TO_RUN}*.bin ]; then
+    pivnet_download_4X ${GPDB_VERSION_TO_RUN}
+
+    if [ -f ${GPDB_4X_ZIP_DIRECTORY}/greenplum-db*${GPDB_VERSION_TO_RUN}*.zip ]; then
         start_gpdb ${GPDB_VERSION_TO_RUN}
     fi
 }
