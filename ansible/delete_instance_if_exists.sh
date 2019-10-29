84b081a83edb26083abf4b2edbdc0dc7da5d0501
Taylor Vesely <tvesely@pivotal.io>
2019-10-29 10:34:49 -0700

    WIP: Install postgres on workstation

--- ansible/delete_instance_if_exists.sh
+++ ansible/delete_instance_if_exists.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-instance_ip=$(gcloud compute instances list --project=${PROJECT} --filter="name='${INSTANCE_NAME}' zone:(${ZONE})" --format='get(networkInterfaces[0].accessConfigs[0].natIP)')
+instance_ip=$(gcloud compute instances list --project=${ANSIBLE_GCP_PROJECT} --filter="name='${ANSIBLE_GCP_INSTANCE_NAME}' zone:(${ANSIBLE_GCP_ZONE})" --format='get(networkInterfaces[0].accessConfigs[0].natIP)')
 
 if [ "${instance_ip}" != "" ]; then
-  gcloud compute instances delete pg-benchmark --project=${PROJECT} --zone=${ZONE}
+  gcloud compute instances delete $ANSIBLE_GCP_INSTANCE_NAME --project=${ANSIBLE_GCP_PROJECT} --zone=${ANSIBLE_GCP_ZONE}
   ssh-keygen -f ~/.ssh/known_hosts -R ${instance_ip}
 fi
